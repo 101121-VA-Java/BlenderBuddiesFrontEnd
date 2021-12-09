071412f8c734d6f6f35f98b6b814c3b1f0a3ce70
adarvin14 <amdarvin14@gmail.com>
2021-12-09 16:35:13 -0500

    made flowchart, create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SmoothiesComponent } from './smoothies/smoothies.component';
+import { UsersComponent } from './users/users.component';
+import { AdminsComponent } from './admins/admins.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SmoothiesComponent,
+    UsersComponent,
+    AdminsComponent,
+    RecipeComponent,
+    IngredientsComponent
   ],
   imports: [
     BrowserModule,
