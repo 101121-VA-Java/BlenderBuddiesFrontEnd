f5f9e4f519f3f1c29b56037301ea24d904a30edb
adarvin14 <amdarvin14@gmail.com>
2021-12-15 17:00:17 -0500

    redirects WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { IngrFormComponent } from './components/ingr-form/ingr-form.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { SmoothieFormComponent } from './components/smoothie-form/smoothie-form.component';
+import { SmoothiesComponent } from './components/smoothies/smoothies.component';
 import { UserDashComponent } from './components/user-dash/user-dash.component';
 
 
@@ -19,6 +22,14 @@ const routes: Routes = [{
 {
   path: 'register',
   component: RegisterComponent
+},
+{
+  path: 'smoothie-form',
+  component: SmoothieFormComponent
+},
+{
+  path: 'inger-form',
+  component: IngrFormComponent
 }];
 
 @NgModule({
