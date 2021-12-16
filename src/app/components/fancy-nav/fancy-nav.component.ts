8a1addfd2e20e66a33ffa3045d66bab1462aa322
adarvin14 <amdarvin14@gmail.com>
2021-12-16 17:55:00 -0500

    nav rework WIP

---
+++ src/app/components/fancy-nav/fancy-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fancy-nav',
+  templateUrl: './fancy-nav.component.html',
+  styleUrls: ['./fancy-nav.component.css']
+})
+export class FancyNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
