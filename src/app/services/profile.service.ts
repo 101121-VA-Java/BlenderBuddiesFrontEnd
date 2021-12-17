4df214e977c3df1b3feff373a58cacb236e2c22a
adarvin14 <amdarvin14@gmail.com>
2021-12-17 15:49:22 -0500

    profile update form added, redir to update form done

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
+
+const API_URL = 'http://localhost:8080/';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +13,13 @@ export class ProfileService {
 
   constructor(private http: HttpClient) { }
 
-  getUser(name: string){
-    console.log(name)
-    this.http.get<any>(`http://localhost:8080/users/{id}`).subscribe(data=>
-    {
-      this.user = data.total;
-    })
-    return this.user;
-  }
+  // getUser(name: string){
+  //   console.log(name)
+  //   this.http.get<any>(API_URL + 'users/{id}', { responseType: 'text' }).subscribe(data=>
+  //   {
+  //     this.user = data;
+  //   })
+  //   return this.user;
+  // }
 
 }
