a6626e068ed3428311cb188d460ddee34e13e82a
adarvin14 <amdarvin14@gmail.com>
2021-12-21 20:03:41 -0500

    fixed update profile form

--- src/app/components/profile-update/profile-update.component.ts
+++ src/app/components/profile-update/profile-update.component.ts
@@ -17,6 +17,17 @@ export class ProfileUpdateComponent implements OnInit {
   constructor(private authService: AuthService, private profileService: ProfileService, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.displayInfo()
+  }
+  profile?: any
+  displayInfo(){
+    let id = this.authService.getUser();
+    this.profileService.getUserById(id).subscribe((response) => {
+      this.profile = response;
+      console.log(this.profile);
+    });
+    
+    
   }
 
   updateUser(firstName: string, lastName: string, username: string, password: string, email: string) {
